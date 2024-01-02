c501a9de0e14241f487ddc89ef8dd373c09a0562
Hewr Srood <Hewr_s@outlook.com>
2024-01-03 01:41:38 +0300

    Refactor carsReducer in client/src/redux/reducers/carsReducer.js

--- client/src/redux/reducers/carsReducer.js
+++ client/src/redux/reducers/carsReducer.js
@@ -1,21 +1,18 @@
 const initialData = {
-    cars: []
-  };
-  
- export const carsReducer = (state = initialData, action) => {
+  cars: [],
+};
+
+export const carsReducer = (state = initialData, action) => {
   switch (action.type) {
-    case 'GET_ALL_CARS':
+    case "GET_ALL_CARS":
       return {
         ...state,
-        cars: action.payload // Set the cars array directly to the action payload
+        cars: action.payload, // Set the cars array directly to the action payload
       };
-    case 'UPDATE_CAR':
+    case "UPDATE_CAR":
       // Logic to update a specific car in the state
       return state; // Return the updated state
     default:
       return state;
   }
 };
- 
-
-
